a09780bb224ff44b55b5d5332d932bbaef904d83
unknown <xie_kevin@sina.com>
2018-07-27 23:28:25 +0800

    refactor config

--- server/config/development.js
+++ server/config/development.js
@@ -1,7 +1,7 @@
 export default {
     NODE_ENV: 'development',
 	db: {
-		uri: 'mongodb://localhost:27017/mevn_local'
+		uri: 'mongodb://localhost:27017/mevn_dev'
 	},
 	cookieSecret: process.env.cookieSecret || 'development',
 }
--- server/config/local.js
+++
@@ -1,7 +0,0 @@
-export default {
-    NODE_ENV: 'local',
-	db: {
-		uri: 'mongodb://localhost:27017/mevn_local'
-	},
-	cookieSecret: process.env.cookieSecret || 'local',
-}
