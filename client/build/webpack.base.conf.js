64a4708f3c8b965d9169025c285333066321fc9f
kevin xie <kevin.xie@cargosmart.com>
2018-07-27 14:33:27 +0800

    add lodash using webpack.ProvidePlugin

--- client/build/webpack.base.conf.js
+++ client/build/webpack.base.conf.js
@@ -3,6 +3,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+const webpack = require('webpack')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -13,7 +14,8 @@ function resolve (dir) {
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/main.js'
+    app: './src/main.js',
+    // vendor: 'lodash'
   },
   output: {
     path: config.build.assetsRoot,
@@ -78,5 +80,10 @@ module.exports = {
     net: 'empty',
     tls: 'empty',
     child_process: 'empty'
-  }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      _: 'lodash'
+    })
+  ],
 }
